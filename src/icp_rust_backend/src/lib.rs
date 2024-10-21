f34754857abc341d163113bca08be2c03a9808c2
Raad05 <yamin.raad6109@gmail.com>
2024-10-21 19:21:44 +0600

    implemented Exam struct, Storable and BoundedStorable

--- src/icp_rust_backend/src/lib.rs
+++ src/icp_rust_backend/src/lib.rs
@@ -1,4 +1,30 @@
 use candid::{CandidType, Decode, Deserialize, Encode};
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
-use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap, Storable};
+use ic_stable_structures::{BoundedStorable, DefaultMemoryImpl, StableBTreeMap, Storable};
 use std::{borrow::Cow, cell::RefCell};
+
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+
+const MAX_VALUE_SIZE: u32 = 100;
+
+#[derive(CandidType, Deserialize)]
+struct Exam {
+    out_of: u8,
+    course: String,
+    curve: u8,
+}
+
+impl Storable for Exam {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+impl BoundedStorable for Exam {
+    const MAX_SIZE: u32 = MAX_VALUE_SIZE;
+    const IS_FIXED_SIZE: bool = false;
+}
